3447d893ae9ac75360b693d9c93bf9d988545cbc
Alonsenio <jalonsolucasr@gmail.com>
2023-11-01 18:11:01 -0500

    probando repositorio en github

---
+++ src/proyectocursointegrador/ProyectoCursoIntegrador.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectocursointegrador;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ProyectoCursoIntegrador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Creando Repositorio");
+    }
+    
+}
