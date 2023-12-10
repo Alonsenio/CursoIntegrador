0a941de2fd258a5d762a2434a632689bfba27c46
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2023-12-09 20:15:22 -0500

    Script SQL actualizado

--- src/Recursos/bddistribuidoragas.sql
+++ src/Recursos/bddistribuidoragas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-11-2023 a las 19:36:38
--- Versión del servidor: 10.4.25-MariaDB
--- Versión de PHP: 8.1.10
+-- Tiempo de generación: 10-12-2023 a las 02:14:47
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,16 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `clientes`
+-- Estructura de tabla para la tabla `almacen`
 --
 
-CREATE TABLE `clientes` (
+CREATE TABLE `almacen` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `dirección` varchar(200) DEFAULT NULL,
-  `teléfono` varchar(20) DEFAULT NULL,
-  `correo` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `proveedor_id` int(11) DEFAULT NULL,
+  `producto_id` int(11) DEFAULT NULL,
+  `fecha_entrada` date DEFAULT current_timestamp(),
+  `precio_compra` decimal(10,2) DEFAULT NULL,
+  `cantidad_unitaria` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `almacen`
+--
+
+INSERT INTO `almacen` (`id`, `proveedor_id`, `producto_id`, `fecha_entrada`, `precio_compra`, `cantidad_unitaria`) VALUES
+(6, 1, 1, '2023-12-07', 495.00, 11),
+(19, 6, 6, '2023-12-07', 100.00, 19),
+(20, 6, 6, '2023-12-07', 100.00, 20),
+(21, 6, 6, '2022-12-07', 100.00, 10),
+(22, 1, 1, '2023-12-07', 4500.00, 100),
+(26, 1, 1, '2023-12-07', 9000.00, 100),
+(27, 1, 5, '2023-12-07', 10200.00, 200);
 
 -- --------------------------------------------------------
 
@@ -47,7 +61,7 @@ CREATE TABLE `detalles_venta` (
   `producto_id` int(11) DEFAULT NULL,
   `cantidad` int(11) DEFAULT NULL,
   `subtotal` decimal(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -61,8 +75,20 @@ CREATE TABLE `productos` (
   `descripción` text DEFAULT NULL,
   `precio` decimal(10,2) DEFAULT NULL,
   `stock` int(11) DEFAULT NULL,
-  `proveedor_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `categoria` varchar(60) DEFAULT NULL,
+  `almacen` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `nombre`, `descripción`, `precio`, `stock`, `categoria`, `almacen`) VALUES
+(1, 'Gas Z', 'balon de 15kg', 45.00, 110, 'Balones 15kg', 'Sección A'),
+(4, 'gas 22', 'dfdfsd', 23.00, 23, '23', 'sfsdf'),
+(5, 'gas 77777', 'fewfe', 34.00, 223, 'wfefewf', 'wdwefew'),
+(6, 'gas 9999', 'efewfewfew', 34.00, 1, 'ewfew', 'wefewf'),
+(14, 'gas hoy', 'efwefwef', 10.00, 80, '30', 'efewf');
 
 -- --------------------------------------------------------
 
@@ -76,7 +102,15 @@ CREATE TABLE `proveedores` (
   `dirección` varchar(200) DEFAULT NULL,
   `teléfono` varchar(20) DEFAULT NULL,
   `correo` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `proveedores`
+--
+
+INSERT INTO `proveedores` (`id`, `nombre`, `dirección`, `teléfono`, `correo`) VALUES
+(1, 'GRAÑA Y MONTERO PETROLERA S.A.\r\n', '123 Flores, Breña, Lima, Perú.', '956879987', 'graña_montero@gamil.com'),
+(6, 'pedro', 'wfwpf wf', '777777777', 'juan@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -87,7 +121,15 @@ CREATE TABLE `proveedores` (
 CREATE TABLE `rol` (
   `id` int(11) NOT NULL,
   `nombreRol` varchar(60) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`id`, `nombreRol`) VALUES
+(1, 'Administrador'),
+(2, 'Cliente');
 
 -- --------------------------------------------------------
 
@@ -99,7 +141,18 @@ CREATE TABLE `rol_usuarios` (
   `id` int(11) NOT NULL,
   `rol_id` int(11) DEFAULT NULL,
   `usuarios_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `rol_usuarios`
+--
+
+INSERT INTO `rol_usuarios` (`id`, `rol_id`, `usuarios_id`) VALUES
+(1, 1, 2),
+(9, 2, 3),
+(10, 2, 4),
+(11, 1, 5),
+(12, 2, 6);
 
 -- --------------------------------------------------------
 
@@ -117,7 +170,18 @@ CREATE TABLE `usuarios` (
   `fecha_modificacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `creado_por` int(11) DEFAULT NULL,
   `modificado_por` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `contraseña`, `activo`, `correo`, `fecha_creacion`, `fecha_modificacion`, `creado_por`, `modificado_por`) VALUES
+(2, 'Alonso', 'hola', 1, 'U21231837@utp.edu.pe', '2023-11-09 17:13:05', '2023-11-09 17:13:05', NULL, NULL),
+(3, 'Anthony', 'hola', 1, 'anthony2003@gmail.com', '2023-11-10 22:38:05', '2023-11-10 22:38:05', NULL, NULL),
+(4, 'Elder', 'hola', 1, 'elder2002@gmail.com', '2023-11-10 22:53:33', '2023-11-10 22:53:33', NULL, NULL),
+(5, 'Pooul', 'hola', 1, 'pooul2003@gmail.com', '2023-11-11 16:42:18', '2023-11-11 16:42:18', 2, NULL),
+(6, 'Juan', 'hola', 1, 'juan@gmail.com', '2023-12-04 18:54:05', '2023-12-04 18:54:05', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -130,17 +194,19 @@ CREATE TABLE `ventas` (
   `fecha_venta` date DEFAULT curdate(),
   `cliente_id` int(11) DEFAULT NULL,
   `total_venta` decimal(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `clientes`
+-- Indices de la tabla `almacen`
 --
-ALTER TABLE `clientes`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `almacen`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `proveedor_id` (`proveedor_id`),
+  ADD KEY `producto_id` (`producto_id`);
 
 --
 -- Indices de la tabla `detalles_venta`
@@ -154,8 +220,7 @@ ALTER TABLE `detalles_venta`
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `proveedor_id` (`proveedor_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `proveedores`
@@ -195,82 +260,83 @@ ALTER TABLE `ventas`
 --
 
 --
--- AUTO_INCREMENT de la tabla `clientes`
+-- AUTO_INCREMENT de la tabla `almacen`
 --
-ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `almacen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT de la tabla `detalles_venta`
 --
 ALTER TABLE `detalles_venta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `proveedores`
 --
 ALTER TABLE `proveedores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `rol_usuarios`
 --
 ALTER TABLE `rol_usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `almacen`
+--
+ALTER TABLE `almacen`
+  ADD CONSTRAINT `fk_proveedor_productos_productos` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_proveedor_productos_proveedores` FOREIGN KEY (`proveedor_id`) REFERENCES `proveedores` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Filtros para la tabla `detalles_venta`
 --
 ALTER TABLE `detalles_venta`
-  ADD CONSTRAINT `detalles_venta_ibfk_1` FOREIGN KEY (`venta_id`) REFERENCES `ventas` (`id`),
-  ADD CONSTRAINT `detalles_venta_ibfk_2` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`);
-
---
--- Filtros para la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`proveedor_id`) REFERENCES `proveedores` (`id`);
+  ADD CONSTRAINT `detalles_venta_ibfk_1` FOREIGN KEY (`venta_id`) REFERENCES `ventas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detalles_venta_ibfk_2` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `rol_usuarios`
 --
 ALTER TABLE `rol_usuarios`
-  ADD CONSTRAINT `rol_usuarios_ibfk_1` FOREIGN KEY (`rol_id`) REFERENCES `rol` (`id`),
-  ADD CONSTRAINT `rol_usuarios_ibfk_2` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`);
+  ADD CONSTRAINT `rol_usuarios_ibfk_1` FOREIGN KEY (`rol_id`) REFERENCES `rol` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `rol_usuarios_ibfk_2` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`);
+  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
